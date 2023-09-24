26ab5d2e05c08c7fa20d719cabdb69dd0d333983
MarianaMorin <morinigo_mariana@hotmail.com>
2023-09-24 15:09:10 -0300

    explicaciones

---
+++ test.py
@@ -0,0 +1,14 @@
+# Operandos normales
+# Resta -
+# Suma +
+# Multiplicacion *
+# Division /
+
+# Operandos booleanos 
+# and
+# or
+# igualdad ==
+# comparativo >
+# comparativo <
+
+# Guarda archivo con CTRL + S
